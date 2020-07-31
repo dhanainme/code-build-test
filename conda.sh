77c5a3666a03b25203b1e5ed57379ce4907f358e
Dhanasekar Karuppasamy <dhanak@amazon.com>
2020-07-31 01:26:33 +0000

    Conda

---
+++ conda.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Go to home directory
+cd ~
+
+# You can change what anaconda version you want at
+# https://repo.continuum.io/archive/
+wget https://repo.continuum.io/archive/Anaconda3-4.2.0-Linux-x86_64.sh
+bash Anaconda3-4.2.0-Linux-x86_64.sh -b -p ~/anaconda
+rm Anaconda3-4.2.0-Linux-x86_64.sh
+echo 'export PATH="~/anaconda/bin:$PATH"' >> ~/.bashrc
+
+# Reload default profile
+source ~/.bashrc
+
+conda update conda
